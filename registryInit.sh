cd4418db75a9e28aa92de312b937598bec5c1ccd
asus <836585692@qq.com>
2022-05-12 16:18:48 +0800

    5.12 v2.8 通过dockerfile制作注册中心镜像，发布到dockerHub，用户可以根据指令一键启动

--- registryInit.sh
+++ registryInit.sh
@@ -6,5 +6,5 @@ docker pull 836585692/zytregistry:1.0
 # 根据镜像创建容器 映射对应的端口 即可使用
 docker run -it -d -p 8848:8848 -p 2181:2181  --restart always 836585692/zytregistry:1.0
 
-# 也可以一键启动
+# 可以选择可以一键启动  但是建议还是分布 更加了解熟悉docker的运用
 docker pull 836585692/zytregistry:1.0&&docker run -it -d -p 8848:8848 -p 2181:2181  --restart always 836585692/zytregistry:1.0
